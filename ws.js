48956368f3b6557dd17a9870c5f3bb186a1ea0b8
drdyne <r.chaumard@gmail.com>
2018-10-26 16:00:59 -0400

    easy extending messages in ws.js

--- ws.js
+++ ws.js
@@ -1,13 +1,24 @@
 const WebSocket = require('ws')
 
+const onMessage = ({send, ...ws} , {type, author, content, ...message}, {clientIP, ...user}) => {
+  switch(type) {
+    case 'roll': {
+      console.log(`/roll ${content}`)
+      // api call or sql operation
+      // send(...something) 
+    }
+  }
+}
+
 const defaultOptions = {
   port: 8081,
   onClose: f=>f, 
   onConnect: f=>f,
-  onMessage: f=>f,
+  onMessage,
   onOpen: f=>f, 
 }
-const messages = {
+
+const Messages = {
   system: { type: 'system' },
   gm: { type: 'gm' },
   whisper: { type: 'whisper', from: undefined, to: undefined },
@@ -21,29 +32,43 @@ const listen = (options=defaultOptions) => {
   const server = new WebSocket.Server({ port })
   console.log(`server listening on port ${port}`)
 
-  server.on('close', () => {
-    console.log('disconnected')
-    onClose(server)
-  })
 
   server.on('connection', (ws, req) => {
     const clientIP = req.connection.remoteAddress
+    const user = { clientIP, }
+
     console.log(`new connection from ${clientIP}`)
-    ws.send(JSON.stringify({type: 'system', content: 'connected'}))
+    ws.send(JSON.stringify({...Messages.system, content: 'connected'}))
     onConnect(ws)
 
+    //broadcast
+    server.clients.forEach(client => {
+      if ( client === ws ) return;
+      if ( client.readyState === WebSocket.OPEN ) 
+        client.send(JSON.stringify({
+          ...Messages.system,
+          content: `${clientIP} connected`
+        }))
+    })
+
     ws.on('message', event => {
-      console.log('on message')
-      console.log('/raw', event)
+      console.log('received message')
+      console.log(`raw/ ${event}`)
       const message = JSON.parse(event)
-      console.log('/parsed', message)
-      onMessage(ws, message)
+      console.log('parsed/', message)
+
+      onMessage(ws, message, user)
 
       //broadcast
       server.clients.forEach(client => {
         if ( client === ws ) return;
         if ( client.readyState === WebSocket.OPEN ) client.send(event)
       })
+    })
+
+    ws.on('close', () => {
+      console.log('disconnected')
+      onClose(ws)
     })
   })
 
